22116a4d3ad6a297ec8ca644685094a6d7338425
Paris Nakita Kejser <parisnakitakejser@Pariss-MacBook.local>
2016-12-20 18:33:11 +0100

    (update) light - return data and validate

--- api/controller/light.php
+++ api/controller/light.php
@@ -6,11 +6,28 @@ class Obj {
       $num = $_POST['num'];
     }
 
-    $on = $_POST['light'];
+    if(isset($_POST['light']) && ($_POST['light'] == 'on' || $_POST['light'] == 'off')) {
+      $on = $_POST['light'];
 
-    $return = json_decode(shell_exec('python ../services/philips-hue/app.py mode=light light='. $on .' num='. $num));
+      $return = json_decode(shell_exec('python ../services/philips-hue/app.py mode=light light='. $on .' num='. $num));
+      if(count($return) > 0) {
 
-    return $return;
+        return [
+          'content' => $return,
+          'status' => 200
+        ];
+      } else {
+        return [
+          'content' => _('There are none lights found right now.'),
+          'status' => 404
+        ];
+      }
+    } else {
+      return [
+        'content' => _('You need to tell what shut happen with the light.'),
+        'status' => 404
+      ];
+    }
   }
 }
 ?>
